4b72ebfdb964dc80e80066647947c2e83b3ce0b9
changhwan hwang <ch.hwang@ecubelabs.com>
2022-10-29 18:53:16 +0900

    feat: add findByIds

--- src/libs/ddd/repository.ts
+++ src/libs/ddd/repository.ts
@@ -2,6 +2,7 @@ import { Inject } from '@nestjs/common';
 import {
   EntityManager,
   FindManyOptions,
+  In,
   ObjectLiteral,
   ObjectType,
 } from 'typeorm';
@@ -78,5 +79,27 @@ export abstract class Repository<T extends ObjectLiteral, ID> {
       this.entityClass,
       spec.where as FindManyOptions<T>,
     );
+  }
+
+  /**
+   *
+   * @param ids
+   */
+  public async findByIds(ids: ID[]) {
+    const primaryColumns = this.entityManager.connection
+      .getMetadata(this.entityClass)
+      .columns.filter((column) => column.isPrimary);
+    if (primaryColumns.length !== 1) {
+      throw new Error(
+        `Not supported: there should be only one primary column on aggregate root, but ${primaryColumns.length} columns are found.`,
+      );
+    }
+
+    const primaryColumnPropertyName = primaryColumns[0].propertyName;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-expect-error
+    return this.entityManager.findBy(this.entityClass, {
+      [primaryColumnPropertyName]: In(ids),
+    });
   }
 }
