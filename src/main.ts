7d2430137b0e1d089fc07fbbd06efdbe394fc085
changhwan hwang <ch.hwang@ecubelabs.com>
2022-10-31 20:29:02 +0900

    feat: docker setting

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { dataSource } from './libs/orm';
 
+const port = process.env.PORT;
+
 async function bootstrap() {
   dataSource.initialize().then(() => console.log('DB Connected 🔥'));
 
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(Number(port)).then(() => console.log('Server Connected 🙏'));
 }
 bootstrap();
