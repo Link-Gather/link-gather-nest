647c62bf1df6b467c53377942c8c47a5f227fc8e
changhwan hwang <ch.hwang@ecubelabs.com>
2022-11-19 17:50:30 +0900

    feat: add exceptionFilter

---
+++ src/libs/exceptionFilter/index.ts
@@ -0,0 +1,20 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  // eslint-disable-next-line class-methods-use-this
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { getConfig } from './config';
+import { HttpExceptionFilter } from './libs/exceptionFilter';
 import { dataSource } from './libs/orm';
 
 const port = getConfig('/port');
@@ -9,6 +10,7 @@ async function bootstrap() {
   dataSource.initialize().then(() => console.log('DB Connected 🔥'));
 
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(Number(port)).then(() => console.log('Server Connected 🙏'));
 }
 bootstrap();
