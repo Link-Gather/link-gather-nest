0cc30028e209382393bd428e832abf4a26d4189e
changhwan hwang <ch.hwang@ecubelabs.com>
2022-11-20 14:02:35 +0900

    feat: add exceptions

--- src/libs/exception/exceptions.ts
+++ src/libs/exception/exceptions.ts
@@ -1,7 +1,22 @@
 import { HttpException } from '@nestjs/common';
 import type { ErrorOption } from './index';
 
-export function badRequest(message?: string, option?: ErrorOption) {
+export const badRequest = (message?: string, option?: ErrorOption) => {
   message = message || 'Bad Request';
   throw new HttpException({ message, errorMessage: option?.errorMessage }, 400);
-}
+};
+
+export const forbidden = (message?: string, option?: ErrorOption) => {
+  message = message || 'Forbidden';
+  throw new HttpException({ message, errorMessage: option?.errorMessage }, 403);
+};
+
+export const unauthorized = (message?: string, option?: ErrorOption) => {
+  message = message || 'Unauthorized';
+  throw new HttpException({ message, errorMessage: option?.errorMessage }, 401);
+};
+
+export const notImplemented = (message?: string, option?: ErrorOption) => {
+  message = message || 'Not Implemented';
+  throw new HttpException({ message, errorMessage: option?.errorMessage }, 501);
+};
