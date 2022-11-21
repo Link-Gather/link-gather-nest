d14e454e8c1ad7219712e76b41d1b16bf853dec8
Windy <ydg0330@naver.com>
2022-11-21 17:38:45 +0900

    feat: create GracefulShutdownService

---
+++ src/libs/graceful-shutdown/index.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnApplicationShutdown } from '@nestjs/common';
+import { dataSource } from '../orm';
+
+@Injectable()
+export class GracefulShutdownService implements OnApplicationShutdown {
+  // eslint-disable-next-line class-methods-use-this
+  async onApplicationShutdown() {
+    console.log('The server shuts down when the connection is cleaned up.');
+
+    try {
+      await dataSource.destroy();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      console.log('bye 👋');
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { getConfig } from './config';
+import { GracefulShutdownService } from './libs/graceful-shutdown';
 import { dataSource } from './libs/orm';
 
 const port = getConfig('/port');
@@ -9,6 +10,10 @@ async function bootstrap() {
   dataSource.initialize().then(() => console.log('DB Connected 🔥'));
 
   const app = await NestFactory.create(AppModule);
+
+  app.get(GracefulShutdownService);
+  app.enableShutdownHooks(['SIGINT', 'SIGTERM']);
+
   await app.listen(Number(port)).then(() => console.log('Server Connected 🙏'));
 }
 bootstrap();
