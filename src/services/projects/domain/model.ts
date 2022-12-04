41ca07f093c1a42edd7eb436ca49758115ecafab
changhwan hwang <ch.hwang@ecubelabs.com>
2022-12-04 23:18:58 +0900

    feat: add project entity

--- src/services/projects/domain/model.ts
+++ src/services/projects/domain/model.ts
@@ -1,13 +1,23 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { customAlphabet } from 'nanoid';
 import { Aggregate } from '../../../libs/ddd/aggregate';
-import { CreateProjectDto } from '../dto/create-project-dto';
+import type { CreateProjectDto } from '../dto/create-project-dto';
 
+const nanoid = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_', 20);
 export const statusType = <const>['recruiting', 'progressing', 'finish', 'close'];
 export type StatusType = typeof statusType[number];
+export const purposeType = <const>['For Improvement', 'For Practice', 'For Fun'];
+export type PurposeType = typeof purposeType[number];
+
+type RecruitMemberType = {
+  developer: number;
+  designer: number;
+  productManager: number;
+};
 
 @Entity()
 export class Project extends Aggregate {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryColumn()
   id!: string;
 
   @Column()
@@ -19,12 +29,39 @@ export class Project extends Aggregate {
   @Column()
   status!: StatusType;
 
+  @Column()
+  purpose!: PurposeType;
+
+  @Column()
+  isRecruiting!: boolean;
+
+  @Column('simple-json')
+  recruitMember!: RecruitMemberType;
+
+  @Column()
+  period!: string;
+
+  @Column('simple-array', { nullable: true })
+  stacks?: string[];
+
+  @Column({ type: 'date', nullable: true })
+  startDate?: Date;
+
+  @Column({ type: 'date', nullable: true })
+  endDate?: Date;
+
   constructor(args: CreateProjectDto) {
     super();
     if (args) {
+      this.id = nanoid();
       this.title = args.title;
       this.description = args.description;
+      this.purpose = args.purpose;
+      this.recruitMember = args.recruitMember;
+      this.period = args.period;
+      this.stacks = args.stacks;
       this.status = 'recruiting';
+      this.isRecruiting = true;
     }
   }
 }
--- src/services/projects/dto/create-project-dto.ts
+++ src/services/projects/dto/create-project-dto.ts
@@ -1,5 +1,21 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsArray, IsIn, IsNotEmpty, IsNumber, IsString, Min, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
+import { purposeType, PurposeType, statusType, StatusType } from '../domain/model';
+
+class RecruitMemberDto {
+  @IsNumber()
+  @Min(0)
+  developer!: number;
+
+  @IsNumber()
+  @Min(0)
+  designer!: number;
+
+  @IsNumber()
+  @Min(0)
+  productManager!: number;
+}
 
 export class CreateProjectDto {
   @ApiProperty({ example: 'title', description: '프로젝트 제목' })
@@ -11,4 +27,25 @@ export class CreateProjectDto {
   @IsNotEmpty()
   @IsString()
   description!: string;
+
+  @ApiProperty({ example: 'For Fun', description: '프로젝트 목적' })
+  @IsNotEmpty()
+  @IsIn(purposeType)
+  purpose!: PurposeType;
+
+  @ApiProperty({ example: { developer: 3, designer: 1, productManager: 1 }, description: '프로젝트 정원' })
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => RecruitMemberDto)
+  recruitMember!: RecruitMemberDto;
+
+  @ApiProperty({ example: '1 개월', description: '프로젝트 기간' })
+  @IsNotEmpty()
+  @IsString()
+  period!: string;
+
+  @ApiProperty({ example: ['node.js', 'react', 'spring'], description: '기술스택', required: false })
+  @IsArray()
+  @IsString({ each: true })
+  stacks?: string[];
 }
