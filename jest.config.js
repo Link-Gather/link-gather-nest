edeeca5f1233334cbed026e62d739b48cccbc60a
Windy <ydg0330@naver.com>
2022-12-19 14:48:20 +0900

    fix: remove unused setting

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,6 @@ module.exports = {
   testRegex: ['.*\\.test\\.ts$'],
   testPathIgnorePatterns: ['/node_modules/'],
   setupFiles: ['./jest.setup.js'],
-  // Referred https://github.com/jest-community/jest-extended
-  //   setupFilesAfterEnv: ['./src/lib/jest-extended'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
