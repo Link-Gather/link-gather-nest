3f5891be21779e6700b6bae0a0a0e081292bbc1c
Windy <ydg0330@naver.com>
2023-01-22 15:32:04 +0900

    fix: add FIXME

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   dataSource.initialize().then(() => console.log('DB Connected 🔥'));
 
   const app = await NestFactory.create(AppModule);
+  // FIXME: 옵션추가
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
