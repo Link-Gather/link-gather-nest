f6d1813941f64f20ba592213a46eb318f87449a9
Windy <ydg0330@naver.com>
2023-01-22 15:31:12 +0900

    fix: getConfig

--- src/services/auth/module.ts
+++ src/services/auth/module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { getConfig } from '../../config';
 import { UserRepository } from '../users/infrastructure/repository';
 import { AuthService } from './application/service';
 import { AuthController } from './presentation/controller';
 
+const jetSecret = getConfig('/jetSecret');
 @Module({
   imports: [
     JwtModule.register({
-      secret: process.env.JWT_SECRET,
+      secret: jetSecret,
     }),
   ],
   controllers: [AuthController],
