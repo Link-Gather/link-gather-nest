d4e0f0840c0ef81563005108a5702aa8b6110782
Windy <ydg0330@naver.com>
2023-01-27 17:15:19 +0900

    fix: remove validation

--- src/services/auth/dto/response-dto.ts
+++ src/services/auth/dto/response-dto.ts
@@ -1,22 +1,12 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ResponseDto {
-  @ApiProperty({ example: 'testehaGeHWfeXCBlNYpiCW2Mwcode', description: 'oauth code', required: false })
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
+  @ApiProperty({ example: 'test@email.com', description: '유저 email' })
   email?: string;
 
   @ApiProperty({ example: 'example-access-token', description: 'user access token', required: false })
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
   accessToken?: string;
 
   @ApiProperty({ example: 'example-refresh-token', description: 'user refresh token', required: false })
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
   refreshToken?: string;
 }
