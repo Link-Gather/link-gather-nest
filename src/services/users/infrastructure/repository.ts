a5a7e5398ae8684dae74b42116993591d00fcb81
Windy <ydg0330@naver.com>
2023-02-20 17:30:20 +0900

    refactor: signUp dto & user repo

--- src/services/users/infrastructure/repository.ts
+++ src/services/users/infrastructure/repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { convertOptions, FindOrder, In, PaginationOption } from '../../../libs/orm';
 import { Repository } from '../../../libs/ddd';
-import { JobType, Profile, User } from '../domain/model';
+import { JobType, Profile, ProviderType, User } from '../domain/model';
 import { stripUndefined } from '../../../libs/common';
 
 @Injectable()
@@ -9,7 +9,7 @@ export class UserRepository extends Repository<User, User['id']> {
   entityClass = User;
 
   async find(
-    conditions: { email?: string; profiles?: { jobs?: JobType[] } },
+    conditions: { email?: string; profiles?: { jobs?: JobType[] }; provider?: ProviderType },
     options?: PaginationOption,
     order?: FindOrder,
   ): Promise<User[]> {
@@ -20,6 +20,7 @@ export class UserRepository extends Repository<User, User['id']> {
           profiles: {
             job: In(conditions.profiles?.jobs),
           },
+          provider: conditions.provider,
         }),
       },
       ...convertOptions(options),
