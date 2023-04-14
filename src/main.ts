62f9166de77d9553c185ecf9285b3c68e4a5972f
Windy <ydg0330@naver.com>
2023-04-14 12:11:47 +0900

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   dataSource.initialize().then(() => console.log('DB Connected 🔥'));
 
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials: true, origin: CORS_ORIGIN });
+  app.enableCors({ credentials: true, origin: JSON.parse(CORS_ORIGIN) });
   app.use(cookieParser(COOKIE_SIGN));
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
