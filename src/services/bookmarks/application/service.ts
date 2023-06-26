75cbd0548126f115c03fe3b963a9c9c5157a825b
Windy <ydg0330@naver.com>
2023-06-26 22:28:46 +0900

    refactor: bookmarkCountChange logic & fix test code

--- src/services/bookmarks/application/service.ts
+++ src/services/bookmarks/application/service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { ProjectRepository } from '../../projects/infrastructure/repository';
 import { BookmarkRepository } from '../infrastructure/repository';
 import { Bookmark } from '../domain/model';
 import { Transactional } from '../../../libs/orm/transactional';
@@ -6,7 +7,7 @@ import { User } from '../../users/domain/model';
 
 @Injectable()
 export class BookmarkService {
-  constructor(private bookmarkRepository: BookmarkRepository) {}
+  constructor(private bookmarkRepository: BookmarkRepository, private projectRepository: ProjectRepository) {}
 
   async list({ user }: { user: User }) {
     return this.bookmarkRepository.find({ userId: user.id });
@@ -14,19 +15,24 @@ export class BookmarkService {
 
   @Transactional()
   async click({ user }: { user: User }, projectId: string) {
-    const [bookmark] = await this.bookmarkRepository.find({ userId: user.id, projectId });
+    const [[bookmark], project] = await Promise.all([
+      this.bookmarkRepository.find({ userId: user.id, projectId }),
+      this.projectRepository.findOneOrFail(projectId),
+    ]);
 
     if (bookmark) {
       bookmark.delete();
+      project.bookmarkCountChange('down');
 
-      await this.bookmarkRepository.save([bookmark]);
+      await Promise.all([this.bookmarkRepository.save([bookmark]), this.projectRepository.save([project])]);
     } else {
       const newBookmark = new Bookmark({
         userId: user.id,
         projectId,
       });
+      project.bookmarkCountChange('up');
 
-      await this.bookmarkRepository.save([newBookmark]);
+      await Promise.all([this.bookmarkRepository.save([newBookmark]), this.projectRepository.save([project])]);
     }
   }
 }
